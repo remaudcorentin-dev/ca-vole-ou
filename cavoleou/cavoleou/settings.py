2e043e6a178884014cf6a4f5740f415d3918012d
Corentin REMAUD <remaudcorentin.dev@gmail.com>
2025-04-11 17:16:30 +0200

    Update

--- cavoleou/cavoleou/settings.py
+++ cavoleou/cavoleou/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+print("DATABASE_URL =", os.environ.get("DATABASE_URL"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +83,7 @@ WSGI_APPLICATION = 'cavoleou.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
